a7f6e593753870897b9d9cdde762f476fbc92863
heegu123 <kookminkhg@kookmin.ac.kr>
2024-11-22 12:18:59 +0900

    API 기능 구현

--- src/main/java/org/example/service/TaskService.java
+++ src/main/java/org/example/service/TaskService.java
@@ -57,6 +57,41 @@ public class TaskService {
         return this.entityToObject(entity);
     }
 
+    public Task update(Long id, String title, String description, LocalDate dueDate) {
+        var exists = this.getById(id);
+
+        exists.setTitle(Strings.isEmpty(title) ?
+                exists.getTitle() : title);
+        exists.setDescription(Strings.isEmpty(description) ?
+                exists.getDescription() : description);
+
+        exists.setDueDate(Objects.isNull(dueDate) ?
+                exists.getDueDate() : Date.valueOf(dueDate));
+
+        var updated = this.taskRepository.save(exists);
+        return this.entityToObject(updated);
+    }
+
+    public Task updateStatus(Long id, TaskStatus status) {
+        var entity = this.getById(id);
+
+        entity.setStatus(status);
+
+        var saved = this.taskRepository.save(entity);
+
+        return this.entityToObject(saved);
+    }
+
+    public boolean delete(Long id) {
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+            log.error("an error occurred while deleting [{}]", e.toString());
+            return false;
+        }
+        return true;
+    }
+
     private TaskEntity getById(Long id) {
         return this.taskRepository.findById(id)
                 .orElseThrow(() ->
